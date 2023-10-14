963067e424612800cbfb6c50a1d8589b690dbb25
Kamil Matejuk <kamilmatejuk@gmail.com>
2023-10-14 13:54:17 +0200

    init

--- main.py
+++ main.py
@@ -3,14 +3,62 @@ from flask import jsonify
 from decouple import Config, RepositoryEnv
 from flask_socketio import SocketIO
 
+from schemas_request import StartRequest #, validate
 
-def hello(name: str):
-    try:
-        assert name.lower() != "world", "Hello World is to basic"
-    except Exception as ex:
-        return jsonify({"error": "Invalid request schema", "details": str(ex)}), 401
 
+def post_greeting(name: str):
     return f'Hello {name}', 200
+
+
+def validate(cls):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                data = cls(**connexion.request.json)
+                data.validate()
+                return func(data)
+            except Exception as ex:
+                return jsonify({"error": "Invalid request schema", "details": str(ex)}), 401
+        return wrapper
+    return decorator
+
+
+'''
+{
+  "asset": "BTCUSDT",
+  "end_time": 15,
+  "model_url": {
+    "host": "localhost",
+    "port": 4001,
+    "slug": "action"
+  },
+  "signals": [
+    {
+      "name": "price",
+      "url": {
+        "host": "localhost",
+        "port": 4002,
+        "slug": "price/latest/BTCUSDT"
+      }
+    }
+  ],
+  "start_time": 10,
+  "timeframe": "1d"
+}
+'''
+@validate(StartRequest)
+def start_backtest(data):
+    # get all signals for all timeframes
+    # combine signals
+    # for each:
+    #   pass to model and get action
+    #   simulate result
+    #   save progress
+    # save results
+    return {}, 200
+
+# create demo model (just random action)
+# once all this is working, add RL to model
 
 
 config = Config(RepositoryEnv('.env.local'))
---
+++ schemas_request.py
@@ -0,0 +1,47 @@
+import re
+import time
+from dataclasses import dataclass
+
+@dataclass
+class URL:
+    host: str
+    port: int
+    slug: str
+    
+    def validate(self, name: str):
+        assert self.host == "localhost" or \
+            re.match(r'[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}', self.host), \
+            f'{name} URL host has to be localhost or IP address'
+        assert isinstance(self.port, int) and self.port >= 1024 and self.port <= 65_535, \
+            f'{name} URL port has to be between 1024 and 65535'
+        assert len(self.slug) > 0, f'{name} URL slug has to be specified'
+            
+
+@dataclass
+class StartRequest:
+    asset: str
+    timeframe: str
+    start_time: int
+    end_time: int
+    model_url: URL
+    signals: list[dict]
+    
+    def validate(self):
+        assert len(self.asset) > 0, \
+            'Cannot pass empty asset'
+        assert self.timeframe in ['1s', '1m', '1h', '1d'], \
+            'Only supported timeframes are 1s, 1m, 1h, 1d'
+        assert self.start_time > 0, \
+            'Start time should be after 01.01.1970r'
+        assert self.end_time < time.time(), \
+            'End time cannot be in the future'
+        assert self.start_time < self.end_time, \
+            'End time has to be after start time'
+        assert len(self.signals) > 0, \
+            'Signals for model cannot be empty'
+        self.model_url = URL(**self.model_url)
+        self.model_url.validate('Model')
+        for s in self.signals:
+            s['url'] = URL(**s['url'])
+            s['url'].validate('Signal')
+
